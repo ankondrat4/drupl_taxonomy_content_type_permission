866619fbc657e8a69b3702818c76b17d5785f280
ankondrat4 <ankondrat4@gmail.com>
2021-09-08 19:13:37 +0300

    add proverka agree

--- register.php
+++ register.php
@@ -2,34 +2,36 @@
 require_once('config.php');
 
 
-    if (($_POST['firstname']!="") && ($_POST['lastname']!="") && ($_POST['email']!="") && ($_POST['date']!="") && ($_POST['topics']!="") && ($_POST['password']!="") ) {
-        $firstname = $_POST['firstname'];
-        $lastname = $_POST['lastname'];
-        $email = $_POST['email'];
-        $date = $_POST['date'];
 
-        $pass = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    if (($_POST['firstname'] != "") && ($_POST['lastname'] != "") && ($_POST['email'] != "") && ($_POST['date'] != "") && ($_POST['topics'] != "") && ($_POST['password'] != "")) {
+        if ($_POST['agree']!=""){
+            $firstname = $_POST['firstname'];
+            $lastname = $_POST['lastname'];
+            $email = $_POST['email'];
+            $date = $_POST['date'];
 
-        $query = "insert into users(first_name,last_name,mail,birth,pass) values('$firstname','$lastname','$email','$date','$pass')";
+            $pass = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-        $result = mysqli_query($conn, $query);
+            $query = "insert into users(first_name,last_name,mail,birth,pass) values('$firstname','$lastname','$email','$date','$pass')";
 
-        echo "<p> Thanks for signing up! You can log in now.</p>                       
-        <script>
-            setTimeout(function(){
-            location.reload();
-            }, 2000);
-        </script>";
-        /*printf("Запрос SELECT вернул %d строк.\n", mysqli_num_rows($result));
+            $result = mysqli_query($conn, $query);
+
+            echo "<p> Thanks for signing up! You can log in now.</p>                       
+            <script>
+                setTimeout(function(){
+                location.reload();
+                }, 2000);
+            </script>";
+            /*printf("Запрос SELECT вернул %d строк.\n", mysqli_num_rows($result));
 
 
-        while($row = mysqli_fetch_array($result))
-        {
-            echo $row['name']."<br>\n";
-        }
-        */
-    }    else echo "Error! Fill in all required fields for registration!";
-
+            while($row = mysqli_fetch_array($result))
+            {
+                echo $row['name']."<br>\n";
+            }
+            */
+        } else echo "Error! You don't agree for registration!";
+    } else echo "Error! Fill in all required fields for registration!";
 
 
 
